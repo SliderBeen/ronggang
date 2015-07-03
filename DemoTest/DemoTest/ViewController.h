f33cf12e12f43c8c35486364d0895f2006bfa306
rg <363642022@qq.com>
2015-07-03 11:47:24 +0800

    xx

---
+++ DemoTest/DemoTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoTest
+//
+//  Created by qianfeng on 15/7/3.
+//  Copyright (c) 2015年 ronggang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DemoTest/DemoTest/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  DemoTest
+//
+//  Created by qianfeng on 15/7/3.
+//  Copyright (c) 2015年 ronggang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"");
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
